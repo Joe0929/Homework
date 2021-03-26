74e92b10414efcfed9716d127ef94e7a0a12beae
Joe0929 <726389960@qq.com>
2021-03-27 00:24:45 +0800

    pythontest

--- test/testdir/testdir.py
+++ test/testdir/testdir.py
@@ -1,5 +1,5 @@
 import unittest
 if __name__=="__main__":
-    test_dir=r"E:\untitled\test"
+    test_dir= r"/test"
     discover=unittest.defaultTestLoader.discover(test_dir,pattern="test*.py")
     unittest.TextTestRunner(verbosity=2).run(discover)
